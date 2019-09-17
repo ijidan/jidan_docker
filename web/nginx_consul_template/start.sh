e1a825205469a8e29672505d2010e87d7952ee55
tony zhu <394777474@qq.com>
2019-09-17 11:19:10 +0800

    update

--- web/nginx_consul_template/start.sh
+++ web/nginx_consul_template/start.sh
@@ -4,5 +4,5 @@ nohup nginx -g 'daemon off;' &
 
 /bin/consul-template --version
 /bin/consul-template \
-	-consul-addr=consul:8500 \
-	-template="/etc/nginx/conf.d/consul-templates.conf.ctmpl:/etc/nginx/conf.d/consul-templates.conf:nginx -s reload"
+	-consul-addr=consul_server:8500 \
+	-template="/etc/nginx/conf.d/consul-templates.http.ctmpl:/etc/nginx/conf.d/http_service.conf:nginx -s reload"
